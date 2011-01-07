638893a30e4099df294d3773a403b42b8912294b
Pedro Algarvio <ufs@ufsoft.org>
2011-01-07 19:55:05 +0000

    `evafm.__init__` was missing.

---
+++ evafm/__init__.py
@@ -0,0 +1,15 @@
+"""
+Audio Failure Monitor
+=====================
+
+Audio Failure Monitor
+"""
+
+__version__      = '0.1-dev'
+__package_name__ = 'EvNAM'
+__summary__      = "Audio Failure Monitor."
+__author__       = 'Pedro Algarvio'
+__email__        = 'ufs@ufsoft.org'
+__license__      = 'BSD'
+__url__          = 'http://rtp.ufsoft.org/EvNam'
+__description__  = __doc__
